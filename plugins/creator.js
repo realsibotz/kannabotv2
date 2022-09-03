02dbd31cf68d8d603c5e2eef30673c42bdb84664
realsibotz <rilrealsi@gmail.com>
2022-09-04 01:43:31 +0800

    Add files via upload

--- plugins/creator.js
+++ plugins/creator.js
@@ -20,7 +20,7 @@ let handler  = async (m, { conn, command, args, usedPrefix, DevMode }) => {
 //------------ BIO
 let ppown = await conn.profilePictureUrl(nomorown + '@s.whatsapp.net', 'image').catch(_ => hwaifu[1]) 
 let teksbio = `${htki} *BIODATA* ${htka}
-*ɴᴀᴍᴇ:* ⓊⒸⒽⒾⒽⒶ ⓃⒶⓇⓊⓉⓄ
+*ɴᴀᴍᴇ:* Alwi Ezy
 *ᴀɢᴇ:* 20ᵗʰ
 *sᴛᴀᴛᴜs:* single 
 
